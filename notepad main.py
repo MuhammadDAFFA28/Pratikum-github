97356f5ae42286a01c4d936ece8dcb251d5a0550
MuhammadDAFFA28 <daffa.mda28@gmail.com>
2026-05-05 12:12:52 +0700

    Implement add_task function to add new tasks
    Added a function to add new tasks to the task list.

--- notepad main.py
+++ notepad main.py
@@ -6,3 +6,8 @@ def show_task():
     else:
         for i, task in enumerate(tasks):
             print(i+1, task)
+
+def add_task():
+    title = input("Masukkan task: ")
+    tasks.append({"title": title, "status": "belum"})
+    print("Task berhasil ditambahkan!")
